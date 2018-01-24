8a96329012d9cad2c250d7f5cb22f96246978716
ninja33 <zhenyu.huang@qq.com>
2018-01-25 01:51:22 +0800

    update dict list

--- uddt/list.js
+++ uddt/list.js
@@ -1,5 +1,5 @@
 RegisterDictList([
-    '../uddt/baicizhan.js', // cndict.cn (百词斩英汉词典)
-    '../uddt/cndict.js', // cndict.cn (海词英汉词典)
-    '../uddt/collins.js' // collins.com (柯林斯英英词典)
+    'https://rawgit.com/ninja33/anki-online-dict-helper/master/uddt/baicizhan.js', // cndict.cn (百词斩英汉词典)
+    'https://rawgit.com/ninja33/anki-online-dict-helper/master/uddt/cndict.js', // cndict.cn (海词英汉词典)
+    'https://rawgit.com/ninja33/anki-online-dict-helper/master/uddt/collins.js' // collins.com (柯林斯英英词典)
 ]);
