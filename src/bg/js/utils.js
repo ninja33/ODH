b405217e12c1ae3d601e8df8dacc074ba4a854c1
ninja33 <zhenyu.huang@qq.com>
2019-01-25 16:12:26 +0800

    delete sunset script and set default sys script

--- src/bg/js/utils.js
+++ src/bg/js/utils.js
@@ -1,14 +1,7 @@
 function sanitizeOptions(options) {
-    let defaultscripts = [
-        { type: "sys", active: true, remote: false, name: "builtin_encn_Collins", description: "柯林斯词典" },
-        { type: "sys", active: false, remote: true, name: "builtin_encn_Oxford", description: "柯林斯词典" },
-        { type: "sys", active: true, remote: false, name: "encn_Collins", description: "柯林斯词典" },
-        { type: "sys", active: true, remote: false, name: "encn_Oxford", description: "牛津在线" }
-    ];
-
     const defaults = {
-        enabled: false,
-        hotkey: '0', // 0:off , 16:shift, 17:ctrl, 18:alt
+        enabled: true,
+        hotkey: '16', // 0:off , 16:shift, 17:ctrl, 18:alt
         maxcontext: '1',
         maxexample: '2',
         monolingual: '0', //0: bilingual 1:monolingual
@@ -28,10 +21,9 @@ function sanitizeOptions(options) {
         url: '',
         audio: '',
 
-        sysscripts: defaultscripts,
+        sysscripts: 'general_Makenotes,builtin_encn_Collins,cncn_Zdic,encn_Collins',
         udfscripts: '',
         
-        dictLibrary: 'builtin_encn_Collins, builtin_encn_Oxford, encn_Collins, encn_Oxford, encn_Baicizhan, cncn_Zdic, frcn_Eudict, escn_Eudict, rucn_Qianyi',
         dictSelected: '',
         dictNamelist: [],
     };
