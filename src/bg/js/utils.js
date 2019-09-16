c551142ee34abef0ed7b02b39d29550fbc4ea11e
ninja33 <zhenyu.huang@qq.com>
2019-09-16 23:25:20 +0800

    fix cambridge dicts and add some new dicts, bump version to 0.8.7

--- src/bg/js/utils.js
+++ src/bg/js/utils.js
@@ -21,7 +21,7 @@ function sanitizeOptions(options) {
         url: '',
         audio: '',
 
-        sysscripts: 'builtin_encn_Collins,general_Makenotes,cncn_Zdic,encn_Collins',
+        sysscripts: 'builtin_encn_Collins,encn_Collins,encn_Cambridge,encn_Oxford,fren_Cambridge,esen_Spanishdict,decn_Eudict,escn_Eudict,frcn_Eudict',
         udfscripts: '',
 
         dictSelected: '',
